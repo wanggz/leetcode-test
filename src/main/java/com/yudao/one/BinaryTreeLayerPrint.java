68b1311563dbe16cc0efacc61052a014f3247ffa
wanggz <453089894@qq.com>
2020-02-01 14:57:54 +0800

    2

--- src/main/java/com/yudao/one/BinaryTreeLayerPrint.java
+++ src/main/java/com/yudao/one/BinaryTreeLayerPrint.java
@@ -39,12 +39,12 @@ public class BinaryTreeLayerPrint {
 
         Queue<TreeNode> q= new LinkedList<>();             //创建的队列用来存放结点，泛型注意是TreeNode
         q.add(root);
+
         while(!q.isEmpty()){                        //队列为空说明已经遍历完所有元素，while语句用于循环每一个层次
             int count=q.size();
             List<Integer> list=new ArrayList();
             while(count>0){                             //遍历当前层次的每一个结点，每一层次的Count代表了当前层次的结点数目
-                TreeNode temp=q.peek();
-                q.poll();                                        //遍历的每一个结点都需要将其弹出
+                TreeNode temp=q.poll();
                 list.add(temp.val);
                 if(temp.left!=null)q.add(temp.left);      //迭代操作，向左探索
                 if(temp.right!=null)q.add(temp.right);
