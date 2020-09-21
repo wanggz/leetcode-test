48f35d7812fabc385c648bba355c84f5f39cb1d2
wanggz <453089894@qq.com>
2020-09-21 18:28:23 +0800

    1

---
+++ src/main/java/com/yudao/leetcode/tree/Tree2List.java
@@ -0,0 +1,18 @@
+package com.yudao.leetcode.tree;
+
+import com.yudao.leetcode.tree.entry.TreeNode;
+
+/**
+ * 114. 二叉树展开为链表
+ */
+public class Tree2List {
+
+    public static void flatten(TreeNode root) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
