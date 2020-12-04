552ae8050dfa77261373a1b61fddc637a5b1e508
wanggz <453089894@qq.com>
2020-12-04 14:14:27 +0800

    CoinChange

---
+++ src/main/java/com/yudao/leetcode/dynamic/MinimumTotal.java
@@ -0,0 +1,60 @@
+package com.yudao.leetcode.dynamic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 120. 三角形最小路径和
+ *
+ * [
+ *      [2],
+ *     [3,4],
+ *    [6,5,7],
+ *   [4,1,8,3]
+ * ]
+ */
+public class MinimumTotal {
+
+    public static void main(String[] args) {
+        List<List<Integer>> triangle = new ArrayList<>();
+        List<Integer> l1 = new ArrayList<>();
+        l1.add(2);
+        triangle.add(l1);
+        List<Integer> l2 = new ArrayList<>();
+        l2.add(3);
+        l2.add(4);
+        triangle.add(l2);
+        List<Integer> l3 = new ArrayList<>();
+        l3.add(6);
+        l3.add(5);
+        l3.add(7);
+        triangle.add(l3);
+        List<Integer> l4 = new ArrayList<>();
+        l4.add(4);
+        l4.add(1);
+        l4.add(8);
+        l4.add(3);
+        triangle.add(l4);
+
+        MinimumTotal m = new MinimumTotal();
+        System.out.println(m.minimumTotal(triangle));
+
+    }
+
+    public int minimumTotal(List<List<Integer>> triangle) {
+        if(triangle == null || triangle.size() == 0) {
+            return 0;
+        }
+        int [] dq = new int[triangle.size()];
+        for(int i = 0; i< dq.length; i++) {
+            dq[i] = triangle.get(triangle.size() - 1).get(i);
+        }
+        for(int i = triangle.size() - 2; i >= 0; i--){
+            for(int j = 0; j <= i; j++){
+                dq[j] = triangle.get(i).get(j) + Math.min(dq[j],dq[j+1]);
+            }
+        }
+        return dq[0];
+    }
+
+}
